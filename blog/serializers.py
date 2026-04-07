cb844139369dc5ff2b0d690a95a063fb8629b9e5
Nikushka2024 <megrelynika@gmail.com>
2026-04-07 12:32:31 +0300

    serializers for Post and Comment

--- blog/serializers.py
+++ blog/serializers.py
@@ -14,3 +14,22 @@ class UserSerializer(serializers.ModelSerializer):
         # хеширование пароля перед сохранением
         user = User.objects.create_user(**validated_data)
         return user
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'text', 'created_at', 'updated_at']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    # имя автора вместо id
+    author = serializers.ReadOnlyField(source='author.username')
+    # вывод комментов сразу в посте
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'image', 'author', 'comments', 'created_at', 'updated_at']

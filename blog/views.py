8ef588280fff20b58c234bc70a1d33a02b926609
Nikushka2024 <megrelynika@gmail.com>
2026-04-11 23:07:33 +0300

    implement ModelViewSets for User, Post, Comment with access control logic

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User, Post, Comment
+from .serializers import UserSerializer, CommentSerializer, PostSerializer
+from .permissions import IsOwnerOrAdmin
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.action == 'create':
+            # регистрация доступна всем
+            return [permissions.AllowAny()]
+        if self.action in ['update', 'partial_update']:
+            # редактировать может только сам себя или админ
+            return [IsOwnerOrAdmin()]
+        if self.action == 'destroy':
+            # удалять может только админ
+            return [permissions.IsAdminUser()]
+        # читать могут только авторизованные или админы
+        return [permissions.IsAuthenticated()]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            # читать могут все
+            return [permissions.AllowAny()]
+        if self.action == 'create':
+            # создавать только авторизованные
+            return [permissions.IsAuthenticated()]
+        # обновлять и удалять только владелец или админ
+        return [IsOwnerOrAdmin()]
+
+    def perform_create(self, serializer):
+        # автоматически автор поста текущий юзер
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [permissions.AllowAny()]
+        if self.action == 'create':
+            return [permissions.IsAuthenticated()]
+        return [IsOwnerOrAdmin()]
+
+    def perform_create(self, serializer):
+        # текущий юзер автор коммента
+        serializer.save(author=self.request.user)
